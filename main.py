2bd79f7ef264732f0cf268541ecf0cbb6411271b
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-09 23:03:36 +0100

    Added OpenAI's chat completion model Fine Tuning

--- main.py
+++ main.py
@@ -5,24 +5,21 @@ from decouple import config
 
 openai.api_key = config("OPENAI_API_KEY")
 
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+]
+
 
 # Main method goes here
 def decipher(audio):
-    print(audio)
 
     # Using openAI's speech to text model
     audio_file = open(audio, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print(transcript)
 
     response =  openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": transcript["text"]},
-            #{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            #{"role": "user", "content": "Where was it played?"}
-        ]
+        messages=messages
     )
 
     print(response)
