735b1b22a4fb4a768e2d2d2ac11f53f31915d692
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-09 22:12:30 +0100

    Implemented Gradio's Audio to Text Interface

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ import gradio as gr
 def decipher(audio):
     print(audio)
     return "Decipher Me"
+
+
+# Using Gradio's audio Interface
+interface = gr.Interface(fn=decipher, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text").launch()
+interface.launch()
