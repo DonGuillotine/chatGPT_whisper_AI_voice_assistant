81b692e3563fc0ffa4bcb154843c62e56a74aee8
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-09 22:56:48 +0100

    Added OpenAI's chat completion model

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import gradio as gr
-import openai 
+import openai
 from decouple import config
 
 
-openai.api_key=config("OPENAI_API_KEY")
+openai.api_key = config("OPENAI_API_KEY")
 
 
 # Main method goes here
@@ -14,9 +14,23 @@ def decipher(audio):
     audio_file = open(audio, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     print(transcript)
+
+    response =  openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": transcript["text"]},
+            #{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            #{"role": "user", "content": "Where was it played?"}
+        ]
+    )
+
+    print(response)
+
     return transcript["text"]
 
 
 # Using Gradio's audio Interface
-interface = gr.Interface(fn=decipher, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text").launch()
+interface = gr.Interface(fn=decipher, inputs=gr.Audio(
+    source="microphone", type="filepath"), outputs="text").launch()
 interface.launch()
