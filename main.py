366cdccd25d56c422b22c99286e47b9de454a1e1
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-09 22:35:03 +0100

    Using openAI's speech to text model

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import gradio as gr
 import openai 
+from decouple import config
+
+
+openai.api_key=config("OPENAI_API_KEY")
 
 
 # Main method goes here
 def decipher(audio):
     print(audio)
+
+    # Using openAI's speech to text model
+    audio_file = open(audio, "rb")
+    deciphere = openai.Audio.transcribe("whisper-1", audio_file)
+    print(decipher)
     return "Decipher Me"
 
 
